545b6ef476e2c29107b684be2740c41977a79eef
sreekanth mandala <smandala@ariqt.com>
2023-01-30 04:08:30 +0530

    added get , post and get user by id

---
+++ routes/aliens.js
@@ -0,0 +1,40 @@
+const { Router } = require('express')
+const express = require('express')
+const alien = require('../models/alien')
+const router = express.Router()
+const Alien = require('../models/alien')
+
+router.get('/', async (req, res) => {
+    try {
+        const aliens = await Alien.find()
+        res.json(aliens)
+    } catch (err) {
+        res.send('Error' + err)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const alien = await Alien.findById(req.params.id)
+        res.json(alien)
+    } catch (err) {
+        res.send('Error' + err)
+    }
+})
+
+router.post('/', async (req, res) => {
+    const alien = new Alien({
+        name: req.body.name,
+        tech: req.body.tech,
+        sub: req.body.sub
+    })
+
+    try {
+      const a1 =  await alien.save()
+      res.json(a1)
+    } catch (err) {
+        res.send('Error' + err)
+    }
+})
+
+module.exports = router
